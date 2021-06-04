6df91b03529c6fc87a615b6c6ff3daf857a95a25
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-04 14:49:13 +0100

    Initial commit

---
+++ src/test/java/com/example/demo/Testvalidation.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Testvalidation {
+
+	Validation v;
+	@BeforeEach
+	void initialize()
+	{
+		v=new Validation();
+	}
+	@Test
+	void verifStringValues()
+	{
+		assertTrue(v.detectString(""));
+		
+	}
+	
+	
+}
